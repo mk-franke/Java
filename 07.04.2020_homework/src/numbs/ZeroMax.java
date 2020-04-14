427d5dabdd3f4cc963ac6ed4519ea26026a19d28
mk-franke <mk-franke@mail.ru>
2020-04-14 23:05:50 +0200

    update homework_1

--- 07.04.2020_homework/src/numbs/ZeroMax.java
+++ 07.04.2020_homework/src/numbs/ZeroMax.java
@@ -2,4 +2,8 @@ package numbs;
 
 public class ZeroMax {
 
+    public int[] zeroMax(int[] nums) {
+
+    }
+
 }
--- 07.04.2020_homework/src/numbs/ZeroMaxTest.java
+++ 07.04.2020_homework/src/numbs/ZeroMaxTest.java
@@ -1,5 +1,13 @@
 package numbs;
 
+import org.junit.Test;
+
 public class ZeroMaxTest {
-    
+
+    @Test
+    public void zeroMaxTest() {
+        ZeroMax zm = new ZeroMax();
+
+    }
+
 }
