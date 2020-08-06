be38b55009accce4fbdbe5e5e9088074ea48c96c
mk-franke <mk-franke@mail.ru>
2020-08-06 22:36:19 +0200

    update

--- 06.08.2020_factorization/src/com/telran/Factorization.java
+++ 06.08.2020_factorization/src/com/telran/Factorization.java
@@ -4,17 +4,16 @@ import java.util.List;
 
 public class Factorization {
 
-    public int find(List<Integer> list) {
-        int[] arr = list.stream().mapToInt(Integer::intValue).toArray();
+    public int find(List<Integer> numbers) {
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (Integer n : numbers) {
             int x = 1;
             do {
-                if (x*(x+1) == arr[i]) {
+                if (x * (x + 1) == n) {
                     count++;
                 }
                 x++;
-            } while (x < Math.sqrt(arr[i]));
+            } while (x < Math.sqrt(n));
         }
         return count;
     }
